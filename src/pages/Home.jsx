99f5174673521987a5296e7c16bb55518aae43d6
yodanisesutantio <yodanise66@gmail.com>
2025-01-02 16:21:07 +0700

    I've shorten the MJSF section, and fix some alignment issue, but now, I need to find a supporting image for each item

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,6 @@ import { Link } from 'react-router-dom';
 import Quotes from '../components/Quotes';
 
 const Home = () => {
-  const pRefs = useRef([]);
-  const [lineHeights, setLineHeights] = useState([]);
-
-  useEffect(() => {
-    const heights = pRefs.current.map((p) => (p ? p.offsetHeight * 1.3 : 0)); // Calculate 120% height
-    setLineHeights(heights);
-  }, []);
-
   return (
     <section className="pt-10 pb-16 px-32 flex flex-col flex-grow items-center justify-center">
         <h1 className="text-7xl font-mclaren font-bold text-[#fffbfc] text-center leading-snug">
@@ -28,7 +20,7 @@ const Home = () => {
         </div>
 
         <h1 className="mt-24 text-4xl font-mclaren font-bold text-[#fffbfc] text-center leading-snug">What I really want to do?</h1>
-        <p className="mt-2 w-[34rem] font-nunito font-light text-base text-[#fffbfc]/75 text-center leading-tight">I haven't decided yet, since I love both of them equally. (Oh, do please hover over the image below to reveal a hidden description)</p>
+        <p className="mt-2 w-96 font-nunito font-light text-base text-[#fffbfc]/75 text-center leading-tight">I haven't decided yet, since I love both of them equally. (Oh, do please hover over the image below!)</p>
 
         <div className="mt-8 w-full grid grid-cols-2 gap-4">
           <div className="w-full h-80 rounded overflow-hidden bg-[url('/img/uid.jpeg')] bg-cover bg-bottom saturate-0 hover:saturate-100 group duration-500">
@@ -51,142 +43,148 @@ const Home = () => {
         </div>
 
         <h1 className="mt-24 text-4xl font-mclaren font-bold text-[#fffbfc] text-center leading-snug">My Journey so far</h1>
-        <p className="mt-2 w-[34rem] font-nunito font-light text-base text-[#fffbfc]/75 text-center leading-tight">Come along as I share the steps that brought me here, one chapter at a time.</p>
+        <p className="mt-2 w-96 font-nunito font-light text-base text-[#fffbfc]/75 text-center leading-tight">Come along as I share the steps that brought me here, one chapter at a time.</p>
 
-        <div className="mt-5 grid grid-cols-5 gap-2 items-start w-full">
-          <div className="col-span-2 flex flex-row gap-2 w-full items-center justify-end">
-            <div className="p-3 w-[90%]">
+        <div className="relative flex flex-col gap-6 justify-center items-center mt-8">
+          <div className="absolute top-2 transform border-x border-[#555253] h-[92%]"></div>
+
+          <div className="relative grid grid-cols-[1fr_2.5rem_1fr] gap-5">
+            {/* Date */}
+            <div className="flex justify-end items-stretch">
               <h2 className="font-mclaren font-medium text-2xl text-right text-[#fffbfc]">July, 2002</h2>
             </div>
-            <div className="p-3 w-[10%] flex flex-col items-center relative">
-              <div className="rounded-full w-6 h-6 bg-[#fffbfc] z-20"></div>
-              {/* Vertical Lines */}
-              <div className="absolute border-l border-r border-[#555253]" style={{ height: `${lineHeights[0] || 0}px` }}></div>
+
+            {/* Middle Dots */}
+            <div className="flex justify-center pt-2">
+              <div className="rounded-full w-4 h-4 bg-[#fffbfc]"></div>
+            </div>
+
+            {/* Content */}
+            <div className="">
+              <p className="font-nunito font-light text-base text-justify text-[#fffbfc] leading-snug pt-[5px] pr-24">I was born in July 2002, a curious child eager to explore the world around me.</p>
             </div>
           </div>
 
-          <div className="col-span-3 flex flex-row gap-2 p-3">
-            <p ref={(el) => (pRefs.current[0] = el)} className="font-nunito font-light text-base text-justify text-[#fffbfc] leading-snug pr-24">This is where it all started—July 2002, in the vibrant city of Surabaya. I made my debut into the world, tiny and curious about everything (or at least that’s how I imagine it). Since then, life has been a constant journey of learning, growing, and figuring things out. While I wasn’t solving puzzles back then, I like to think that this was the first milestone in a story that’s still being written.</p>
-          </div>
-        </div>
-
-        <div className="mt-3 grid grid-cols-5 gap-2 items-start w-full">
-          <div className="col-span-2 flex flex-row gap-2 w-full items-center justify-end">
-            <div className="p-3 w-[90%]">
+          <div className="relative grid grid-cols-[1fr_2.5rem_1fr] gap-5">
+            {/* Date */}
+            <div className="flex justify-end items-stretch">
               <h2 className="font-mclaren font-medium text-2xl text-right text-[#fffbfc]">January, 2007</h2>
             </div>
-            <div className="p-3 w-[10%] flex flex-col items-center relative">
-              <div className="rounded-full w-6 h-6 bg-[#fffbfc] z-20"></div>
-              <div className="absolute border-l border-r h-[240%] border-[#555253]" style={{ height: `${lineHeights[1] || 0}px` }}></div>
+
+            {/* Middle Dots */}
+            <div className="flex justify-center pt-2">
+              <div className="rounded-full w-4 h-4 bg-[#fffbfc]"></div>
+            </div>
+
+            {/* Content */}
+            <div className="">
+              <p className="font-nunito font-light text-base text-justify text-[#fffbfc] leading-snug pt-[5px] pr-24">I moved from Surabaya to the neighboring town of Sidoarjo. I didn’t fully understand my feelings about the change, but I learned to adapt and make do with my new surroundings.</p>
             </div>
           </div>
 
-          <div className="col-span-3 flex flex-row gap-2 p-3">
-            <p ref={(el) => (pRefs.current[1] = el)} className="font-nunito font-light text-base text-justify text-[#fffbfc] leading-snug pr-24">Change came knocking in January 2007 when I left Surabaya, the city of my earliest memories, and moved to the neighboring town of Sidoarjo. At that age, I didn’t quite know how to process the shift—it was just something that happened. A new house, new surroundings, and eventually, new experiences. Looking back, it feels like the first lesson in adapting to life’s twists and turns, even when you don’t fully understand them.</p>
-          </div>
-        </div>
-
-        <div className="mt-3 grid grid-cols-5 gap-2 items-start w-full">
-          <div className="col-span-2 flex flex-row gap-2 w-full items-center justify-end">
-            <div className="p-3 w-[90%]">
+          <div className="relative grid grid-cols-[1fr_2.5rem_1fr] gap-5">
+            {/* Date */}
+            <div className="flex justify-end items-stretch">
               <h2 className="font-mclaren font-medium text-2xl text-right text-[#fffbfc]">August, 2014</h2>
             </div>
-            <div className="p-3 w-[10%] flex flex-col items-center relative">
-              <div className="rounded-full w-6 h-6 bg-[#fffbfc] z-20"></div>
-              <div className="absolute border-l border-r h-[240%] border-[#555253]" style={{ height: `${lineHeights[2] || 0}px` }}></div>
+
+            {/* Middle Dots */}
+            <div className="flex justify-center pt-2">
+              <div className="rounded-full w-4 h-4 bg-[#fffbfc]"></div>
+            </div>
+
+            {/* Content */}
+            <div className="">
+              <p className="font-nunito font-light text-base text-justify text-[#fffbfc] leading-snug pt-[5px] pr-24">It marked the start of my junior high years, where I joined multiple organizations and discovered what felt like my first passion—video production.</p>
             </div>
           </div>
 
-          <div className="col-span-3 flex flex-row gap-2 p-3">
-            <p ref={(el) => (pRefs.current[2] = el)} className="font-nunito font-light text-base text-justify text-[#fffbfc] leading-snug pr-24">At the beginning of my junior high school journey, I threw myself into a whirlwind of activities—joining organizations, experimenting with extracurriculars, and even dabbling in video production. Each experience taught me something new, whether it was teamwork, creativity, or just discovering what I enjoyed. It was a vibrant chapter that sparked my curiosity and gave me a glimpse of the things I could be passionate about.</p>
-          </div>
-        </div>
-
-        <div className="mt-3 grid grid-cols-5 gap-2 items-start w-full">
-          <div className="col-span-2 flex flex-row gap-2 w-full items-center justify-end">
-            <div className="p-3 w-[90%]">
+          <div className="relative grid grid-cols-[1fr_2.5rem_1fr] gap-5">
+            {/* Date */}
+            <div className="flex justify-end items-stretch">
               <h2 className="font-mclaren font-medium text-2xl text-right text-[#fffbfc]">July, 2017</h2>
             </div>
-            <div className="p-3 w-[10%] flex flex-col items-center relative">
-              <div className="rounded-full w-6 h-6 bg-[#fffbfc] z-20"></div>
-              <div className="absolute border-l border-r h-[240%] border-[#555253]" style={{ height: `${lineHeights[3] || 0}px` }}></div>
+
+            {/* Middle Dots */}
+            <div className="flex justify-center pt-2">
+              <div className="rounded-full w-4 h-4 bg-[#fffbfc]"></div>
+            </div>
+
+            {/* Content */}
+            <div className="">
+              <p className="font-nunito font-light text-base text-justify text-[#fffbfc] leading-snug pt-[5px] pr-24">My high school years began in July 2017. By my second year, I focused on my Multimedia major, sharpening my video production skills and discovering my passion for UI/UX Design.</p>
             </div>
           </div>
 
-          <div className="col-span-3 flex flex-row gap-2 p-3">
-            <p ref={(el) => (pRefs.current[3] = el)} className="font-nunito font-light text-base text-justify text-[#fffbfc] leading-snug pr-24">Marked the start of an unforgettable chapter—my high school years. These three years were filled with friendships that blossomed into lifelong connections. At first, I took the same approach as junior high, diving into various activities and organizations. But by my second year, I shifted gears to focus on my major, Multimedia.
-
-            <br /> <br />
-              
-            This is where I honed my video production skills and eventually stumbled upon something that truly clicked—UI/UX Design. It felt like finding a path that combined creativity, functionality, and problem-solving, and I’ve been captivated ever since.</p>
-          </div>
-        </div>
-
-        <div className="mt-3 grid grid-cols-5 gap-2 items-start w-full">
-          <div className="col-span-2 flex flex-row gap-2 w-full items-center justify-end">
-            <div className="p-3 w-[90%]">
+          <div className="relative grid grid-cols-[1fr_2.5rem_1fr] gap-5">
+            {/* Date */}
+            <div className="flex justify-end items-stretch">
               <h2 className="font-mclaren font-medium text-2xl text-right text-[#fffbfc]">September, 2020</h2>
             </div>
-            <div className="p-3 w-[10%] flex flex-col items-center relative">
-              <div className="rounded-full w-6 h-6 bg-[#fffbfc] z-20"></div>
-              <div className="absolute border-l border-r h-[240%] border-[#555253]" style={{ height: `${lineHeights[4] || 0}px` }}></div>
+
+            {/* Middle Dots */}
+            <div className="flex justify-center pt-2">
+              <div className="rounded-full w-4 h-4 bg-[#fffbfc]"></div>
+            </div>
+
+            {/* Content */}
+            <div className="">
+              <p className="font-nunito font-light text-base text-justify text-[#fffbfc] leading-snug pt-[5px] pr-24">Despite the global pandemic, September 2020 marked the start of my college journey as an Information Systems student.</p>
+
+              <p className="font-nunito font-light text-base text-justify text-[#fffbfc] leading-snug pt-[5px] pr-24">I honed my UI/UX knowledge and, along the way, stumbled upon Web Development—a discovery that opened up exciting new possibilities.</p>
             </div>
           </div>
 
-          <div className="col-span-3 flex flex-row gap-2 p-3">
-            <p ref={(el) => (pRefs.current[4] = el)} className="font-nunito font-light text-base text-justify text-[#fffbfc] leading-snug pr-24">September 2020 was a defining moment, even though the world had been shaken by the pandemic. I refused to let it stop me from moving forward. It was my first year as a college student, pursuing Information Systems, and it became the year I dove headfirst into the world of UI/UX Design. 
-              
-            <br /> <br />  
-              
-            I also immersed myself in IT Controls and Management through an internship at the Offices of Cooperative and Small Medium Enterprises of East Java, pushing through the challenges. And, in this time of disruption, I discovered Web Development—a whole new realm that expanded my horizons even further. This journey was proof that no matter what the world throws at you, there’s always room to grow, adapt, and keep moving forward.</p>
-          </div>
-        </div>
-
-        <div className="mt-3 grid grid-cols-5 gap-2 items-start w-full">
-          <div className="col-span-2 flex flex-row gap-2 w-full items-center justify-end">
-            <div className="p-3 w-[90%]">
+          <div className="relative grid grid-cols-[1fr_2.5rem_1fr] gap-5">
+            {/* Date */}
+            <div className="flex justify-end items-stretch">
               <h2 className="font-mclaren font-medium text-2xl text-right text-[#fffbfc]">December, 2024</h2>
             </div>
-            <div className="p-3 w-[10%] flex flex-col items-center relative">
-              <div className="rounded-full w-6 h-6 bg-[#fffbfc] z-20"></div>
-              <div className="absolute border-l border-r h-[240%] border-[#555253]" style={{ height: `${lineHeights[5] || 0}px` }}></div>
+
+            {/* Middle Dots */}
+            <div className="flex justify-center pt-2">
+              <div className="rounded-full w-4 h-4 bg-[#fffbfc]"></div>
+            </div>
+
+            {/* Content */}
+            <div className="">
+              <p className="font-nunito font-light text-base text-justify text-[#fffbfc] leading-snug pt-[5px] pr-24">This marked the end of my educational journey with the submission of my final assignment—a web-based app for a driving school using the incremental model.</p>
+              <p className="font-nunito font-light text-base text-justify text-[#fffbfc] leading-snug pt-[5px] pr-24">You can find more details on this project on my <Link to='/projects' className='underline hover:no-underline font-semibold'>Projects</Link> page. While this was a milestone, my journey is far from over, as I continue exploring what’s next.</p>
             </div>
           </div>
 
-          <div className="col-span-3 flex flex-row gap-2 p-3">
-            <p ref={(el) => (pRefs.current[5] = el)} className="font-nunito font-light text-base text-justify text-[#fffbfc] leading-snug pr-24">It marked the conclusion of my educational journey as I submitted my final assignment—a web-based app for a driving school built using the incremental model. It was a project that tested my skills and creativity, for more details on this project, feel free to check out my <Link to="/projects" className="underline hover:no-underline font-semibold">Projects</Link> page. However, despite reaching this milestone, I still find myself exploring and questioning what I truly want to pursue next. And that’s the beauty of this journey—it doesn’t have a clear ending. The path keeps unfolding, and I’m excited to continue exploring where it leads me.</p>
-          </div>
-        </div>
-
-        <div className="mt-3 grid grid-cols-5 gap-2 items-start w-full">
-          <div className="col-span-2 flex flex-row gap-2 w-full items-center justify-end">
-            <div className="p-3 w-[90%]">
+          <div className="relative grid grid-cols-[1fr_2.5rem_1fr] gap-5">
+            {/* Date */}
+            <div className="flex justify-end items-stretch">
               <h2 className="font-mclaren font-medium text-2xl text-right text-[#fffbfc]">Today</h2>
             </div>
-            <div className="p-3 w-[10%] flex flex-col items-center relative">
-              <div className="rounded-full w-6 h-6 bg-[#fffbfc] z-20"></div>
-              <div className="absolute border-l border-r border-dashed h-[240%] border-[#555253]" style={{ height: `${lineHeights[6] || 0}px` }}></div>
+
+            {/* Middle Dots */}
+            <div className="flex justify-center pt-2">
+              <div className="rounded-full w-4 h-4 bg-[#fffbfc]"></div>
+            </div>
+
+            {/* Content */}
+            <div className="">
+              <p className="font-nunito font-light text-base text-justify text-[#fffbfc] leading-snug pt-[5px] pr-24">As of today, I’m open to new opportunities and challenges, with a dream of working abroad—perhaps Germany? The chance to grow my skills while exploring new cultures excites me, and I’m ready for whatever comes next.</p>
             </div>
           </div>
 
-          <div className="col-span-3 flex flex-row gap-2 p-3">
-            <p ref={(el) => (pRefs.current[6] = el)} className="font-nunito font-light text-base text-justify text-[#fffbfc] leading-snug pr-24">As I stand at this point in my journey, I’m open to any opportunities and offers that come my way, excited to explore new paths and challenges. There’s one particular dream on my bucket list though—working in Germany. The idea of gaining experience in a new culture and environment, while further growing my skills, is something I’m truly passionate about. But for now, I remain open to where the next step will take me, knowing that the journey is far from over.</p>
-          </div>
-        </div>
-
-        <div className="mt-3 grid grid-cols-5 gap-2 items-start w-full">
-          <div className="col-span-2 flex flex-row gap-2 w-full items-center justify-end">
-            <div className="p-3 w-[90%]">
+          <div className="relative grid grid-cols-[1fr_2.5rem_1fr] gap-5">
+            {/* Date */}
+            <div className="flex justify-end items-stretch">
               <h2 className="font-mclaren font-medium text-2xl text-right text-[#fffbfc]">May, 2025</h2>
             </div>
-            <div className="p-3 w-[10%] flex flex-col items-center relative">
-              <div className="rounded-full w-6 h-6 bg-[#fffbfc] z-20"></div>
-              <div className="absolute hidden border-l border-r h-[240%] border-[#555253]" style={{ height: `${lineHeights[7] || 0}px` }}></div>
-            </div>
-          </div>
 
-          <div className="col-span-3 flex flex-row gap-2 p-3">
-            <p ref={(el) => (pRefs.current[7] = el)} className="font-nunito font-light text-base text-justify text-[#fffbfc] leading-snug pr-24">I’ll finally earn my degree and be able to proudly call myself Yodanis E. Sutantio, B.Tech. It’s a moment of accomplishment and a reminder that the journey has been worth every step. But as much as it’s a conclusion to my academic chapter, it’s also the beginning of what’s next. I’m ready to see where this newfound title and all the experiences I've gained will take me.</p>
+            {/* Middle Dots */}
+            <div className="flex justify-center pt-2">
+              <div className="rounded-full w-4 h-4 bg-[#010400] border-2 border-[#fffbfc]"></div>
+            </div>
+
+            {/* Content */}
+            <div className="">
+              <p className="font-nunito font-light text-base text-justify text-[#fffbfc] leading-snug pt-[5px] pr-24">I’ll be attending my graduation and officially earning my bachelor’s degree. It’s a proud milestone that closes one chapter while opening doors to new adventures.</p>
+            </div>
           </div>
         </div>
 
