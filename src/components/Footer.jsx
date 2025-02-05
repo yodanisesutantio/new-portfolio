05db2ce4b17ef49bab91f3955a5b433cb711b6cd
yodanisesutantio <yodanise66@gmail.com>
2025-02-05 14:22:00 +0700

    navbar and the footer is responsive now

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,16 +6,16 @@ const Footer = () => {
   const currentYear = new Date().getFullYear(); // Get the current year
 
   return (
-    <div className="flex justify-between items-center p-2">
-      <p className="text-sm font-normal font-nunito text-[#fffbfc]/40">
+    <div className="flex justify-center sm:justify-between items-center p-2">
+      <p className="text-xs md:text-sm font-normal font-nunito text-[#fffbfc]/40">
         © {currentYear} Portfolio by Yodanis E. Sutantio
       </p>
 
-      <ul className="flex space-x-6">
+      <ul className="sm:flex space-x-6 hidden">
         <li>
           <Link
             to="/about"
-            className="flex flex-col font-nunito font-normal text-[#fffbfc]/40 text-sm hover:text-[#fffbfc] duration-500 group"
+            className="flex flex-col font-nunito font-normal text-[#fffbfc]/40 text-xs md:text-sm lg:hover:text-[#fffbfc] duration-500 group"
           >
             About
           </Link>
@@ -23,7 +23,7 @@ const Footer = () => {
         <li>
           <Link
             to="/projects"
-            className="flex flex-col font-nunito font-normal text-[#fffbfc]/40 text-sm hover:text-[#fffbfc] duration-500 group"
+            className="flex flex-col font-nunito font-normal text-[#fffbfc]/40 text-xs md:text-sm lg:hover:text-[#fffbfc] duration-500 group"
           >
             Projects
           </Link>
@@ -31,7 +31,7 @@ const Footer = () => {
         {/* <li>
           <Link
             to="/socials"
-            className="flex flex-col font-nunito font-normal text-[#fffbfc]/40 text-sm hover:text-[#fffbfc] duration-500 group"
+            className="flex flex-col font-nunito font-normal text-[#fffbfc]/40 text-xs md:text-sm lg:hover:text-[#fffbfc] duration-500 group"
           >
             Socials
           </Link>
@@ -39,7 +39,7 @@ const Footer = () => {
         <li>
           <Link
             to="/resume"
-            className="flex flex-col font-nunito font-normal text-[#fffbfc]/40 text-sm hover:text-[#fffbfc] duration-500 group"
+            className="flex flex-col font-nunito font-normal text-[#fffbfc]/40 text-xs md:text-sm lg:hover:text-[#fffbfc] duration-500 group"
           >
             Resume
           </Link>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,42 +7,42 @@ const Navbar = () => {
   const location = useLocation();
 
   return (
-    <nav className="flex justify-between items-center px-32 py-6 sticky top-0 bg-[#010400] z-50 border-b border-[#fffbfc]/10">
-      <div className="flex items-center">
+    <nav className="flex justify-between items-center px-4 sm:px-10 lg:px-32 xl:px-44 py-5 lg:py-6 sticky top-0 w-full bg-[#010400] z-50 border-b border-[#fffbfc]/10">
+      <div className="flex items-center shrink-0">
         <Link to="/">
           <img
             src="/img/yodanisesutantio_logomark.png"
             alt="Yodanis E. Sutantio | Home"
-            className="h-10 rounded-lg hover:shadow-xl hover:shadow-[#fffbfc]/15 duration-500"
+            className="h-9 md:h-10 xl:h-12 rounded-lg lg:hover:shadow-xl lg:hover:shadow-[#fffbfc]/15 duration-500"
           />
         </Link>
       </div>
 
-      <ul className="flex space-x-12">
+      <ul className="flex space-x-8 sm:space-x-12 lg:space-x-12">
         <li>
           <Link
             to="/about"
-            className={`flex flex-col font-nunito font-semibold ${
+            className={`flex flex-col font-nunito font-semibold text-sm md:text-base ${
               location.pathname === "/about"
                 ? "text-[#fffbfc]"
                 : "text-[#fffbfc]/40"
-            } hover:text-[#fffbfc] duration-500 group`}
+            } lg:hover:text-[#fffbfc] duration-500 group`}
           >
             About
-            <span className="w-full h-[3px] bg-linear-to-l from-[#CAFE48] to-[#57467B] transform scale-x-0 transition-transform duration-500 origin-right rounded-full group-hover:scale-x-100"></span>
+            <span className="w-full h-[3px] bg-linear-to-l from-[#CAFE48] to-[#57467B] transform scale-x-0 transition-transform duration-500 origin-right rounded-full lg:group-hover:scale-x-100"></span>
           </Link>
         </li>
         <li>
           <Link
             to="/projects"
-            className={`flex flex-col font-nunito font-semibold ${
+            className={`flex flex-col font-nunito font-semibold text-sm md:text-base ${
               location.pathname === "/projects"
                 ? "text-[#fffbfc]"
                 : "text-[#fffbfc]/40"
-            } hover:text-[#fffbfc] duration-500 group`}
+            } lg:hover:text-[#fffbfc] duration-500 group`}
           >
             Projects
-            <span className="w-full h-[3px] bg-linear-to-l from-[#CAFE48] to-[#57467B] transform scale-x-0 transition-transform duration-500 origin-right rounded-full group-hover:scale-x-100"></span>
+            <span className="w-full h-[3px] bg-linear-to-l from-[#CAFE48] to-[#57467B] transform scale-x-0 transition-transform duration-500 origin-right rounded-full lg:group-hover:scale-x-100"></span>
           </Link>
         </li>
         {/* <li>
@@ -61,14 +61,14 @@ const Navbar = () => {
         <li>
           <Link
             to="/resume"
-            className={`flex flex-col font-nunito font-semibold ${
+            className={`flex flex-col font-nunito font-semibold text-sm md:text-base ${
               location.pathname === "/resume"
                 ? "text-[#fffbfc]"
                 : "text-[#fffbfc]/40"
-            } hover:text-[#fffbfc] duration-500 group`}
+            } lg:hover:text-[#fffbfc] duration-500 group`}
           >
             Resume
-            <span className="w-full h-[3px] bg-linear-to-l from-[#CAFE48] to-[#57467B] transform scale-x-0 transition-transform duration-500 origin-right rounded-full group-hover:scale-x-100"></span>
+            <span className="w-full h-[3px] bg-linear-to-l from-[#CAFE48] to-[#57467B] transform scale-x-0 transition-transform duration-500 origin-right rounded-full lg:group-hover:scale-x-100"></span>
           </Link>
         </li>
       </ul>
